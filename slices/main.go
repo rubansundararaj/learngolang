f6aa01d9f2d7542bbcb370905d45c3922c2e05e9
Ruban Sundara Raj Thasapparaj <ruban.thasapparaj@sivantos.com>
2022-03-18 16:43:30 +0800

    add slices

---
+++ slices/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	fmt.Println("Learn slices")
+
+	var fruitList = []string{"Apple", "Tomato", "peach"}
+	fmt.Printf("%T", fruitList)
+
+	fruitList = append(fruitList, "Mango", "Banana")
+
+	fruitList = append(fruitList[1:])
+	fmt.Println(fruitList)
+
+	highScores := make([]int, 4)
+
+	highScores[0] = 234
+	highScores[1] = 423
+	highScores[2] = 543
+	highScores[3] = 645
+
+	fmt.Println(highScores)
+
+	highScores = append(highScores, 656, 898, 321)
+
+	fmt.Println(highScores)
+
+	sort.Ints(highScores)
+
+	fmt.Println(sort.IntsAreSorted(highScores))
+
+}
