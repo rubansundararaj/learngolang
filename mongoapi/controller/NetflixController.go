d97e215694e4866f89178729f4bdb2bc832f7405
Ruban Sundara Raj Thasapparaj <ruban.thasapparaj@sivantos.com>
2022-03-23 10:43:03 +0800

    partialCurd on movies

--- mongoapi/controller/NetflixController.go
+++ mongoapi/controller/NetflixController.go
@@ -4,7 +4,10 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"mongoapi/model"
 
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -28,4 +31,48 @@ func init() {
 	fmt.Println("Mongodb connection success")
 	collection = client.Database(dbName).Collection(colName)
 	fmt.Println("Collection instance is ready")
+}
+
+func insertOneMovie(movie model.NetFlix) {
+	inserted, err := collection.InsertOne(context.Background(), movie)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Inserted 1 movie", inserted.InsertedID)
+}
+
+func updateOneMovie(movieId string) {
+	id, _ := primitive.ObjectIDFromHex(movieId)
+	filter := bson.M{"_id": id}
+	update := bson.M{"set": bson.M{"watched": true}}
+
+	result, err := collection.UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("modified count: ", result.ModifiedCount)
+}
+
+func deleteOneMovie(movieId string) {
+	id, _ := primitive.ObjectIDFromHex(movieId)
+	filter := bson.M{"_id": id}
+	deleteCount, err := collection.DeleteOne(context.Background(), filter)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Deleted one count : ", deleteCount)
+
+}
+
+func deleteAllMovie() int64 {
+	deleteCollection, err := collection.DeleteMany(context.Background(), bson.D{{}}, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Number of deleted movies : ", deleteCollection.DeletedCount)
+	return deleteCollection.DeletedCount
 }
